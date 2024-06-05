66b6727e8dd0e76e001a6e4a189f9ce208d4d8d6
Gabriely Siqueira <gabrielysiqueira2004@gmail.com>
2024-06-05 15:49:58 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('active');
+    });
+
+    $(window).on('scroll load', function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('active');
+
+        if($(window).scrollTop() > 60){
+            $('.header').addClass('active');
+        }
+        else{
+            $('.header').removeClass('active');
+        }
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        const sections = $('section');
+        const navItems = $('.navbar a');
+
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        });
+
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('.home .content', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.services .box', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+    
+
+    ScrollReveal().reveal('.about .image', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+
+});
